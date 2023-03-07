639b9f25087c95a78111239b51ac2d4873ba1b77
Selasi K <selasiklutsey10@gmail.com>
2023-03-07 12:05:50 +0000

    ABM4

---
+++ src/abm4/agentframework.py
@@ -0,0 +1,40 @@
+import random
+    
+class Agent:
+    def __init__(self, i):
+        self.i = i
+        self.x = random.randint(0, 99)
+        self.y = random.randint(0, 99)
+        pass
+    #string method    
+    def __str__(self):
+        return self.__class__.__name__ + "(x=" + str(self.x) \
+        + ", y=" + str(self.y) + ", i=" + str(self.i) + ")"
+    #repair method
+    def __repr__(self):
+        return str(self)
+    #move method
+    def move(self, x_min, y_min, x_max, y_max):
+        rn =random.random()
+        if rn < 0.5:
+            self.x = self.x + 1
+        else:
+            self.x = self.x - 0
+        #y-coordinate
+        rn = random.random()
+        #print("rn", rn)
+        if rn < 0.5:
+            self.y = self.y  + 1
+        else:
+            self.y  = self.y  - 1
+            
+         # Apply movement constraints.
+        if self.x < x_min:
+         self.x = x_min
+        if self.y  < y_min:
+         self.y  = y_min
+        if self.x > x_max:
+         self.x = x_max
+        if self.y  > y_max:
+         self.y  = y_max
+
